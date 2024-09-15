733bc0b63de083dac9484c04e7f23910e049d9e0
dsang8686 <156335556+dsang8686@users.noreply.github.com>
2024-09-16 06:16:28 +0700

    add

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Login from "./screens/Login/Login";
 import Home from "./screens/Home/Home";
 import Product from "./screens/Products/Product";
 import Update from "./screens/Products/Update";
-import Insert from "./screens/Products/Insert";
+import Insert from "./screens/Insert/Insert";
+import Detail from "./screens/Products/Detail";
+import Category from "./screens/Categotys/Category";
 import Menu from "./screens/Menu/Menu";
 
 import {
@@ -21,12 +23,24 @@ function App() {
     <div>
       <Router>
         <Routes>
+          {/* authen */}
           <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+
           <Route path="/home" element={<Home />} />
 
+          {/* san pham */}
           <Route path="/product" element={<Product />} />
+          <Route path="/detail" element={<Detail />} />
           <Route path="/update-product" element={<Update />} />
+
+
+          {/* them san pham */}
           <Route path="/insert-product" element={<Insert />} />
+
+          {/* danh muc */}
+          <Route path="/category" element={<Category />} />
+
 
           {/* <Route path='/menu' element={<Menu />} /> */}
         </Routes>
--- src/screens/Products/Detail.js
+++ src/screens/Products/Detail.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Menu from "../Menu/Menu";
 
-const Update = () => {
+const Detail = () => {
   return (
     <div className="home-container">
       <Menu />
       <div className="content">
-        <h3>Update</h3>
+        <h3>Detail</h3>
       </div>
     </div>
   );
 };
 
-export default Update;
+export default Detail;
